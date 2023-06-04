7cb519dd1d0adffcdc760b6c081a9d2bf0772780
VitalyBoro <127157980+VitalyBoro@users.noreply.github.com>
2023-06-04 13:22:12 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,33 +1,31 @@
 package jm.task.core.jdbc.service;
-
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-
+import jm.task.core.jdbc.dao.UserDao;
 import java.util.List;
 
 public class UserServiceImpl  implements UserService {
-    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+   public static final UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
-
         userDao.createUsersTable();
     }
-
     public void dropUsersTable() {
         userDao.dropUsersTable();
     }
-
     public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
+        System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
-
     public void removeUserById(long id) {
         userDao.removeUserById(id);
     }
-
     public List<User> getAllUsers() {
+       userDao.getAllUsers();
+       for (User user : userDao.getAllUsers()) {
+           System.out.println(user);
+       }
        return userDao.getAllUsers();
     }
-
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
     }
