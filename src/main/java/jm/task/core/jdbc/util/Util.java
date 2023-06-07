56e98b25b1f694d3ec09a174467a36d3e0c67680
VitalyBoro <127157980+VitalyBoro@users.noreply.github.com>
2023-06-08 01:11:43 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,22 @@
 package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+
 
 public class Util {
     // реализуйте настройку соеденения с БД
+     static SessionFactory sessionFactory;
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/db_1";
     private static final String DB_LOGIN = "root";
@@ -21,4 +33,27 @@ public class Util {
         }
         return conn;
         }
-}
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/db_1?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "springcourse");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                  sessionFactory = (SessionFactory) configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+    }
